77a27e0b826d695589c0d213995b4ef5d1333307
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-01-30 09:04:32 -0600

    fix: add icom from react-icons instead of previous CDN FontAwesome link

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { BiLoaderCircle } from "react-icons/bi";
 
 export default function Loading() {
   return (
-        <div className='flex flex-col flex-1 justify-center items-center'>
-            <i className="fa-solid text-slate-800 fa-spinner animate-spin text-4xl sm:text-5xl"></i>
-        </div>
+    <div className='flex flex-col flex-1 justify-center items-center'>
+      <BiLoaderCircle className='text-slate-800 animate-spin text-4xl sm:text-5xl'/>
+    </div>
   )
 }
